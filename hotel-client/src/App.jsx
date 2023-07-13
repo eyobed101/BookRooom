9b1eeb7ba703dc11a308ecdce2556b039c443632
eyobed101 <eyocjjazz101@gmail.com>
2023-07-13 16:31:03 +0300

    Homepage

---
+++ hotel-client/src/App.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Router, Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+// import LandingPage from "./pages/LandingPage";
+// import LoginPage from "./pages/LoginPage";
+// import RegisterPage from "./pages/RegisterPage";
+// import Rooms from "./pages/Rooms";
+// import SingleRoom from "./pages/SingleRoom";
+// import Error from "./pages/Error";
+import { ThemeProvider, createTheme } from "@mui/material";
+import Navbar from "./components/Navbar.";
+import { orange } from "@mui/material/colors";
+
+
+const theme = createTheme({
+  status: {
+    danger: orange[500],
+  },
+});
+
+function App() {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+      <Navbar />
+      
+
+      
+        <Routes>
+          {/* <Route exact path="/" component={LandingPage} /> */}
+          {/* <Route exact path="/login" component={LoginPage} /> */}
+          {/* <Route exact path="/register" component={RegisterPage} /> */}
+          <Route path="/" element={<Home />} />
+          {/* <Route exact path="/rooms" component={Rooms} /> */}
+          {/* <Route exact path="/rooms/:slug" component={SingleRoom} /> */}
+          {/* <Route exact path="/booknow" component={PaymentForm} /> */}
+          {/* <Route component={Error} /> */}
+        </Routes>
+        </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
---
+++ hotel-client/src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+// import Banner from "../components/Banner";
+// import Hero from "../components/Hero";
+// import Services from "../components/Services";
+
+const Home = () => {
+  return (
+    <div>home</div>
+    //   {/* <Hero> */}
+    //     {/* <Banner
+    //       title="Book rooms"
+    //       subtitle="deluxe rooms in Ethiopia"
+    //     > */}
+    //       {/* <Link to="/rooms" className="btn-primary">
+    //         Our rooms
+    //       </Link> */}
+    //     {/* </Banner> */}
+    //   {/* </Hero> */}
+    //   {/* <Services /> */}
+    
+  );
+};
+
+export default Home;
